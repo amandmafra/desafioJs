9b3be2f6ad9cd9378f2490f62eea1b5e933592e9
amandmafra <135349875+amandmafra@users.noreply.github.com>
2024-02-19 10:22:10 -0300

    Add files via upload

---
+++ projetoDesafio/script.js
@@ -0,0 +1,51 @@
+
+//Exercícios js
+//2.
+/*let numberOne = Number(prompt(`primeiro valor:`))
+let numberTwo = Number(prompt(`segundo valor:`))
+let result = numberOne + numberTwo
+alert(`O resultado da soma é: ${result}`)*/
+
+//3.
+//pra descobrir o tipo de dado pode usar o typeof ou isNaN
+/*let valor = prompt(`Digite um número:`)
+if(!isNaN(valor)){
+  alert(`é um número`)
+}else{
+  alert(`não é um número`)
+}*/
+
+//4.
+/*let string = prompt(`Digite uma string:`)
+if(isNaN(string)){
+  alert(`é uma string`)
+}else{
+  alert(`não é uma string`)
+}*/
+
+//6.
+/*let numberOne = Number(prompt(`Digite um valor`))
+let numberTwo = Number(prompt(`Digite outro valor`))
+
+alert(numberOne - numberTwo)*/
+
+//7.
+/*let numberOne = Number(prompt(`Digite um valor`))
+let numberTwo = Number(prompt(`Digite outro valor`))
+
+alert(numberOne * numberTwo)*/
+
+//8.
+/*let numberOne = Number(prompt(`Digite um valor`))
+let numberTwo = Number(prompt(`Digite outro valor`))
+
+alert(numberOne / numberTwo)*/
+
+//9.
+/*let number = Number(prompt(`Digite um número:`));
+
+if(number % 2 === 0){
+  alert(`o número é par`)
+}else{
+  alert(`o número não é par`)
+}*/
